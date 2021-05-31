2a31f68eeec5c5406568c78c3c8c58d3e6e4fadb
Miguel Bautista <bautistaperez3m@gmail.com>
2021-05-31 13:18:18 +0200

    Cors fixed

--- src/main/java/com/alolorsus/collector/AlbumCollectorApplication.java
+++ src/main/java/com/alolorsus/collector/AlbumCollectorApplication.java
@@ -4,20 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @Configuration
-@ComponentScan(basePackages = {
-        "com.alolorsus.mtgdb",
-        "com.alolorsus.collector"
-    })
+@ComponentScan(basePackages = { "com.alolorsus.mtgdb", "com.alolorsus.collector" })
 public class AlbumCollectorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlbumCollectorApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer CORSConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 
 }
